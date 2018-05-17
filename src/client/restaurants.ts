acc6e069f7c0220804e38809a3d99ea0a8e170d8
Miroslav Kubus <MiroslavK@kentico.com>
2018-05-17 11:27:46 +0200

    KA-141 Change distances of restaurants

--- src/client/restaurants.ts
+++ src/client/restaurants.ts
@@ -1,4 +1,4 @@
-// distance in minutes
+// distance in metres form FI main entrance
 
 type Restaurant = {
     name: string;
@@ -10,11 +10,11 @@ const sortByDistance = ({ distance: distance1 = 0 }: Restaurant, { distance: dis
     distance1 - distance2;
 
 export const restaurants: Restaurant[] = [
-    { name: 'India', id: 16511911, distance: 4 },
-    { name: 'Magistr', id: 16506840, distance: 2 },
-    { name: 'U Dřeváka', id: 16505458, distance: 4 },
-    { name: 'Al Capone', id: 16515833, distnace: 4 },
-    { name: 'Divá Bára', id: 16514047, distance: 6 },
-    { name: 'U Bílého beránka', id: 16506737, distance: 7 },
-    { name: 'Himalaya', id: 18020959, distance: 9 },
+    { name: 'Lights of India', id: 16511911, distance: 400 },
+    { name: 'Magistr', id: 16506840, distance: 170 },
+    { name: 'U Dřeváka', id: 16505458, distance: 300 },
+    { name: 'Al Capone', id: 16515833, distnace: 300 },
+    { name: 'Divá Bára', id: 16514047, distance: 450 },
+    { name: 'U Bílého beránka', id: 16506737, distance: 550 },
+    { name: 'Himalaya', id: 18020959, distance: 800 },
 ].sort(sortByDistance);
